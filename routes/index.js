6a659b66ad9df07319eb6a65ce55b23cc44e272e
Wangzhetuteng <ericdicaprio@outlook.com>
2016-02-10 21:38:37 -0500

    Customize Pages

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('home', { title: 'Yandong Wang' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
+});
+
+/* GET skills page. */
+router.get('/skills', function(req, res, next) {
+  res.render('skills', { title: 'Skills' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 
 module.exports = router;
